6fc3b49b129de110ab7410aa7b7e28501d188f10
raydover <105178034+raydover@users.noreply.github.com>
2022-06-13 22:46:11 -0400

    add additional files needed

---
+++ Develop/badmath.js
@@ -0,0 +1,8 @@
+const pie = "apple";
+
+const predictable = ;
+
+module.exports = {
+    pie: 'apple',
+    predictable: () =>1,
+};
--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
+const badmath = require('./Develop/badmath.js');
+
+console.log(badmath.pie);
+
+console.log(badmath.predictable());
 
 // TODO: Create an array of questions for user input
+console.log(process.argv[2]);
+
 const questions = [];
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your user name?',
+            name: 'name',
+        },
+        {
+            type: 'checkbox',
+            message: 'What language do you know?',
+            name: 'language',
+            choices: ['JS', 'HTML', 'CSS'],
+        },
+        {
+            type: 'list',
+            message: 'What is your preferred method of communication?',
+            name: 'preferred',
+            choices: ['phone', 'email', 'text'],
+        },
+    ])
+
+    // .then((response) => {
+    //     console.log('Success!')
+    //     console.log(response)
+
+    .then((data) => {
+        const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+        fs.writeFile(filename, JSON.stringify(data, null, 2), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
+
+    // });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
